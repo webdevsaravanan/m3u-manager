7b03bafbbfabd593ba8ac59ea89b808933a65480
webdevsaravanan <webdevsaravanan@gmail.com>
2026-04-25 14:10:19 +0530

    updated token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  githubToken: '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',
+  githubToken: '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',
   gistId: 'e56d8cfe808c1ee7325ade32a426afd8',
   githubUsername: 'webdevsaravanan',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // ── FILL THESE IN ──────────────────────────────────────────
-  githubToken: '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',       // GitHub PAT (gist scope only)
+  githubToken: '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',       // GitHub PAT (gist scope only)
   gistId: 'e56d8cfe808c1ee7325ade32a426afd8',                 // e.g. a1b2c3d4e5f6...
   githubUsername: 'webdevsaravanan', // e.g. saravana
   // ───────────────────────────────────────────────────────────
